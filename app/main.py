51b363a76d75d9fe0df35f0559faa72a892fa4e2
observer04 <tripathysarojinee@gmail.com>
2025-06-07 12:13:16 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,13 +5,16 @@ def main():
     while True:
         # Uncomment this block to pass the first stage
         sys.stdout.write("$ ")
-
+        sys.stdout.flush() 
         # Wait for user input
         command= input()
-        if command == "exit 0":
-            break
-        print(f"{command}: command not found")
-        
+        com_and_args= command.split(" ")
+        if com_and_args[0] == "exit":
+            sys.exit(0)
+        elif com_and_args[0] == "echo":
+            print(" ".join(com_and_args[1:]))
+        else:
+            print(f"{command}: command not found")
     
 
 
